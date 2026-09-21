50b853bf8fc8cea2396461dcb769dd57294f5ba6
Sudipta Sarkar <23je0974@iitism.ac.in>
2026-09-22 04:14:20 +0530

    Prisma set up with user model and initial migration

---
+++ server/prisma/migrations/20260921201543_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "program" TEXT NOT NULL,
+    "branch" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "avatarUrl" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'active',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
